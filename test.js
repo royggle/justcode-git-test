fe5f364901c61fe92779a807cd256004982135b9
royggle <younoroy@gmail.com>
2022-08-13 01:01:35 +0900

    [ADD] git quiz submit

---
+++ test.js
@@ -0,0 +1,11 @@
+function countEvenNum() {
+  let arr = [];
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+console.log(countEvenNum());
